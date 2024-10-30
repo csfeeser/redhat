4ffd3bbfc593b8694ae87df5238923eca74ff159
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2024-10-29 20:54:02 -0400

    Update day3setup.sh

--- scripts/day3setup.sh
+++ scripts/day3setup.sh
@@ -4,10 +4,10 @@
 tuned-adm profile balanced
 
 # Disable SELinux persistently
-sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+# sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 
 # Start a high CPU usage process named "big-ol-cpu-burner"
 bash -c 'exec -a big-ol-cpu-burner dd if=/dev/zero of=/dev/null' &
 
 # Start a low CPU usage process named "lil-ol-me"
-bash -c 'exec -a lil-ol-me sleep 1000' &
+# bash -c 'exec -a lil-ol-me sleep 1000' &
